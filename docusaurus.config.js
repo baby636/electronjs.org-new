d20d5f1719a2df1fda9c21aae4033cc1ccadfe91
Erick Zhao <erick@hotmail.ca>
2021-06-02 00:51:14 -0700

    feat: add npm2yarn plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,5 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
+const npm2yarn = require('@docusaurus/remark-plugin-npm2yarn');
 const fiddleEmbedder = require('./src/transformers/fiddle-embedder.js');
 
 module.exports = {
@@ -98,7 +99,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/electron/electronjs.org-new',
-          remarkPlugins: [fiddleEmbedder],
+          remarkPlugins: [fiddleEmbedder, [npm2yarn, { sync: true }]],
         },
         blog: false,
         theme: {
